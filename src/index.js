88b1369894c08fdeb34fec439affb08783b36f79
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-04-16 18:24:01 -0300

    Test routes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,43 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (req, res) => {
-  return res.json('Hello World');
+  return res.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
 });
 
-app.listen(3333);
+app.post('/projects', (req, res) => {
+  return res.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.post('/projects/', (req, res) => {
+  return res.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.put('/projects/:id', (req, res) => {
+  return res.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  return res.json([
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.listen(3333, () => {
+  console.log('☢️   Shiiiieeeeet...')
+});
